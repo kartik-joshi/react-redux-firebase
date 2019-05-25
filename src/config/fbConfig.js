581402dee90cfafce56714ea1013ed1dd2f60e89
Kartik Joshi <kartikjoshi82@yahoo.com>
2019-05-24 18:09:39 -0700

    update project fixed, backgroun image removed

---
+++ src/config/fbConfig.js
@@ -0,0 +1,13 @@
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Replace this with your own config details
+var config = {
+    // your key 
+  };
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase 
